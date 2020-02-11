9de472674c4a661d701e5c57d49fcdb2e01693e8
Vadym Borodin <borodean@gmail.com>
2020-02-12 01:57:05 +0200

    Limit the concurrency to 1

--- karma.conf.js
+++ karma.conf.js
@@ -47,6 +47,7 @@ module.exports = config => {
   if (!config.local) {
     config.set({
       browsers: Object.keys(customLaunchers),
+      concurrency: 1,
       customLaunchers,
       reporters: [...config.reporters, 'saucelabs']
     });
